56e2d8be7919457c6f8ba2593259f1866ec2d320
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-13 16:54:45 +0100

    Whitelabel error remise

--- kir/src/main/java/sacha/kir/MainErrorController.java
+++ kir/src/main/java/sacha/kir/MainErrorController.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-public class MainErrorController implements ErrorController {
+//@Controller
+public class MainErrorController {//implements ErrorController {
 
-	@RequestMapping(value = "/error", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/error", method = RequestMethod.GET)
     public String handleError()
     {
     	return "errorPage";
@@ -17,5 +17,5 @@ public class MainErrorController implements ErrorController {
     @Override
     public String getErrorPath() {
         return "/error";
-    }
+    }*/
 }
