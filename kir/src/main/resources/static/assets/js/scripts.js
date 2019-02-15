426dc195782415ef736ae72c295f3c764a7a5932
WihT <th.witczak@gmail.com>
2019-02-15 14:46:49 +0100

    nettoyage js

--- kir/src/main/resources/static/assets/js/scripts.js
+++ kir/src/main/resources/static/assets/js/scripts.js
@@ -1,45 +1,3 @@
-/*$(document).ready(function()
-{
-	$("#notificationLink").click(function()
-	{
-		$("#notificationContainer").fadeToggle(300);
-		$("#notification_count").fadeOut("slow");
-		return false;
-	});
-			//Document Click hiding the popup 
-			$(document).click(function()
-			{
-				$("#notificationContainer").hide();
-			});
-			//Popup on click
-			$("#notificationContainer").click(function()
-			{
-				return false;
-			});
-
-});
-
-$("#notificationLink1").click(function()
-{
-	window.location.href="https://www.google.fr/"
-});
-
-$(function(){
-	$("form").submit(function(e) {
-		e.preventDefault();
-		var $form = $(this);
-		$form.submit();
-		.done(function(data) {
-			$("#formulaire").modal("hide"); 
-		    $('#success_msg').modal('show');
-			$("#html").html(data);
-		})
-		.fail(function() {
-			alert("Erreur d'envoi");
-		});
-	});
-});*/
-
 $(document).ready(function()
 {
 	$("#notificationLink").click(function()
@@ -54,7 +12,7 @@ $(document).ready(function()
 	{
 		$("#notificationContainer").hide();
 	});
-			
+	
 	//Popup on click
 	$("#notificationContainer").click(function()
 	{
@@ -65,4 +23,41 @@ $(document).ready(function()
 	{
 		window.location.href="https://www.google.fr/"
 	});
+
+	
+
+	
 });
+
+function myFunction() {
+	var x = document.getElementById("mdp");
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+}
+
+
+function sendData()
+{
+	$.ajax(
+	{
+		type: "POST",
+		data: $("#addUserForm").serialize(),
+		cache: false,
+		url: "/adminAdd",
+		success: function(data) 
+		{
+
+			$("#formulaire").modal("hide"); 
+			$('#success_msg').modal('show');
+
+		},
+		error: function()
+		{
+			alert("Error - Data not saved");
+		}
+
+	});
+}
