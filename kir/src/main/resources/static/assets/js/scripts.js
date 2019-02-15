49cc9e6652c0ece19697b6f49e928e18b737bda6
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-15 14:26:58 +0100

    Ptn Thibaut WTF ???

--- kir/src/main/resources/static/assets/js/scripts.js
+++ kir/src/main/resources/static/assets/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function()
+/*$(document).ready(function()
 {
 	$("#notificationLink").click(function()
 	{
@@ -37,5 +37,32 @@ $(function(){
 		.fail(function() {
 			alert("Erreur d'envoi");
 		});
+	});
+});*/
+
+$(document).ready(function()
+{
+	$("#notificationLink").click(function()
+	{
+		$("#notificationContainer").fadeToggle(300);
+		$("#notification_count").fadeOut("slow");
+		return false;
+	});
+	
+	//Document Click hiding the popup 
+	$(document).click(function()
+	{
+		$("#notificationContainer").hide();
+	});
+			
+	//Popup on click
+	$("#notificationContainer").click(function()
+	{
+		return false;
+	});
+	
+	$("#notificationLink1").click(function()
+	{
+		window.location.href="https://www.google.fr/"
 	});
 });
