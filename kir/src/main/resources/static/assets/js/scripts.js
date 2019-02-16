f6200b58cb445db7c7baa3095c96766db552a05a
WihT <th.witczak@gmail.com>
2019-02-16 19:35:07 +0100

    amélioration visuelle formNDF

--- kir/src/main/resources/static/assets/js/scripts.js
+++ kir/src/main/resources/static/assets/js/scripts.js
@@ -44,12 +44,10 @@ function sendData()
 		data: $("#addUserForm").serialize(),
 		cache: false,
 		url: "/adminAdd",
-		success: function(data) 
+		success: function() 
 		{
-
 			$("#formulaire").modal("hide"); 
 			$('#success_msg').modal('show');
-
 		},
 		error: function()
 		{
@@ -57,13 +55,55 @@ function sendData()
 		}
 	});
 }
- /*
-function deleteUser(id) 
+/*
+function successShow()
+{
+	$.ajax(
+	{
+		cache: false,
+		url: "/roleChanged",
+		success: function() 
+		{ 
+			alert("Error - Data not saved");
+		},
+		error: function()
+		{
+			alert("Error - Data not saved");
+		}
+	});
+}
+
+$('#warning_msg').on('show.bs.modal', function (e) {
+  $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+  $('#deleteVersionHiddenId').val($(this).find('.btn-ok').attr('href'));
+});
+
+$('#deleteVersionHiddenbtn').click(function (e){
+  e.preventDefault();
+  var Id = $('#deleteVersionHiddenId').val();
+  $.ajax({
+        type: "DELETE",
+        data: Id,
+        cache: false,
+        url: "/deleteUser/",
+        success: function() {
+        	$("#warning_msg").modal("hide"); 
+        	alert("User deleted");
+        }
+		error: function()
+		{
+			alert("Error - Data not saved");
+		}
+    });
+});
+
+
+function deleteUser() 
 {
     $.ajax({
         type: "POST",
         cache: false,
-        url: "/deleteUser/"+id,
+        url: "/deleteUser/"+$('#deleteVersionHiddenId').val(),
         success: function() {
         	$("#warning_msg").modal("hide"); 
         	alert("User deleted");
@@ -74,3 +114,27 @@ function deleteUser(id)
 		}
     });
 }*/
+
+$(function() {
+  $(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+  });
+
+  $(document).ready( function() {
+      $(':file').on('fileselect', function(event, numFiles, label) {
+
+          var input = $(this).parents('.input-group').find(':text'),
+              log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+          if( input.length ) {
+              input.val(log);
+          } else {
+              if( log ) alert(log);
+          }
+
+      });
+  });
+});
