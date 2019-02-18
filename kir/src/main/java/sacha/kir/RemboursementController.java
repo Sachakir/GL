cfc938a191b50110d911606e56fbec2646aaa7ba
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-18 21:46:21 +0100

    Changement de "mon compte" pour nom prenom (nav bar)

--- kir/src/main/java/sacha/kir/RemboursementController.java
+++ kir/src/main/java/sacha/kir/RemboursementController.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -71,6 +72,16 @@ public class RemboursementController {
     InterfaceAppRoleService AppRoleService;
 	@Autowired
 	InterfaceUserRoleService UserRoleService;
+	
+	@ModelAttribute("username")
+	public String getUsername(Principal principal) {
+		if(principal != null)
+		{
+			String[] name = principal.getName().split("\\.");
+			return name[0] + " " + name[1];
+		}
+		else return "";
+	}
 	
 	@GetMapping(value = {"/", ""})
 	public String displayNotesFrais(Model model, Principal principal) {
